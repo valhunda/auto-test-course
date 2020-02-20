a918e21c89dd990da5775bcc414adcbc87b29e93
valhunda <kismanaa@mail.ru>
2020-02-20 18:00:14 +0400

    Add decision for lesson 3.2, step 8

--- les_3_2_step_8.py
+++ les_3_2_step_8.py
@@ -1,3 +1,9 @@
 def test_input_text(expected_result, actual_result):
     assert actual_result == expected_result, \
         f"expected {expected_result}, got {actual_result}"
+
+
+def checking_function_test_input_text():
+    x = input()
+    y = input()
+    test_input_text(x, y)
